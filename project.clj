51af8a7a958f428af800d593408fa02b5d96870a
Justin Heyes-Jones <justinhj@gmail.com>
2014-02-23 21:08:16 -0800

    update project for clojars

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject justinhj/monger-session "1.0.0"
   :description "mongodb as http session storage via monger"
+  :url "https://github.com/justinhj/monger-session"
   :dependencies [[org.clojure/clojure "1.5.1"]
 		 [ring/ring-core "1.2.1"]
                  [com.novemberain/monger "1.7.0"]])
